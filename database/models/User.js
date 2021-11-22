e5b0eb6c55ef7eaa4e9a6a24a89d0348e08dd3bc
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-22 16:51:49 +0100

    added User schema

---
+++ database/models/User.js
@@ -0,0 +1,30 @@
+const {Schema, model, Types} = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },  
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  favourites: {
+    type: [Types.ObjectId],
+    ref: "Announcement",
+    default: [],
+  },
+  adverts: {
+    type: [Types.ObjectId],
+    ref: "Announcement",
+    default: [],
+  }, 
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
