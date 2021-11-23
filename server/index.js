8b6a1ba80b331e53f9dbd3a03dd2fd2c0cb2bf14
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-23 10:13:24 +0100

    Defined server function

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const debug = require('debug')('file:server:index');
+const chalk = require('chalk');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const usersRoutes = require('./routes/usersRoutes');
+
+const app = express();
+
+let server;
+const initializeServer = (port) => new Promise((resolve, reject) => {
+  server = app.listen(port, () => {
+    debug(chalk.yellow(`Listening to port ${port}`));
+    resolve(server);
+  });
+
+  server.on('error', (error) => {
+    debug(chalk.red('Error when initializing server'));
+    if (error.code === 'EADDRINUSE') {
+      debug(chalk.red(`The port ${port} is in use`));
+    }
+    reject(error);
+  });
+});
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', usersRoutes);
+
+module.exports = { app, initializeServer };
