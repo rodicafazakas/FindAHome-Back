1fbbf627a7b9da5643301b4ab060a7f12cc575b5
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-11-25 11:47:39 +0100

    Added tests of announcementsControllers

--- server/controllers/announcementsControllers.js
+++ server/controllers/announcementsControllers.js
@@ -32,7 +32,6 @@ const createAnnouncement = async (req, res, next) => {
     error.code = 403;
     next(error);
   }
-
   try {
     const newAnnouncement = await Announcement.create(req.body);
     res.status(200).json(newAnnouncement);
@@ -48,7 +47,6 @@ const updateAnnouncement = async (req, res, next) => {
     error.code = 403;
     next(error);
   }
-
   debug(chalk.yellow("Modify announcement at /announcements/id"));
   const { id } = req.params;
   try {
